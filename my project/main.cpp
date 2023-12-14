214da90a04dcdf2a622904f3df45e3fa72fc812d
amr <amrkaled222@gmail.com>
2023-12-14 21:09:11 +0200

    amrrr

--- my project/main.cpp
+++ my project/main.cpp
@@ -4,15 +4,19 @@ using namespace std;
 int sum(int a,int b){
 return a+b;
 }
-
+int amr(int v){
+return v*3;
+}
 int main()
 {
     cout << "Hello world!" << endl;
     cout << "my name is amr" << endl;
     cout << 4 << endl;
     cout << sum(4,5) << endl;
+
     return 0;
 }
 int sub(int a,int b){
 return a-b;
 }
+
